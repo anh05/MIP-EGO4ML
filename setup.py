a647b22a76d81bcaef902d4294a88a1de7679b28
Duc Anh Nguyen <38556234+anh05@users.noreply.github.com>
2019-12-12 13:26:27 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='mipego4ml',
-    version='0.1',
+    name='MIP-EGO4ML',
+    version='0.0.1',
     packages=['mipego4ml', 'mipego4ml.mipego', 'mipego4ml.mipego.optimizer'],
     url='hyperparameter.ml',
-    license='',
+    license='MIT',
     author='Duc Anh Nguyen, Hao Wang, Thomas Back',
     author_email='d.a.nguyen@liacs.leidenuniv.nl',
-    description='This project is a extension of MIP-EGO project',
+    description='This project is an extension of MIP-EGO project',
     install_requires=['pandas', 'numpy', 'scipy', 'scikit-learn', 'joblib', 'dill']
-
 )
